c99f7e25a4e89fbcd114e8913d61bff8f64a9906
Samkit Sheth <samkitsheth95@gmail.com>
2020-12-29 23:45:29 -0800

    CSS Theaming setup done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,40 @@
+import React from 'react';
+import Background from './Background/Background';
 import './App.css';
 
 function App() {
+  const [currentTheme, setTheme] = React.useState('light');
+
+  const lightTheme = {
+    '--color-primary': '#ffffff',
+  };
+  const darkTheme = {
+    '--color-primary': '#171c28',
+  };
+
+  const applyTheme = (nextTheme) => {
+    const theme = nextTheme === 'dark' ? lightTheme : darkTheme;
+    Object.keys(theme).map((key) => {
+      const value = theme[key];
+      // eslint-disable-next-line no-undef
+      document.documentElement.style.setProperty(key, value);
+      return '';
+    });
+  };
+
   return (
     <div className="App">
-        home
+      <button
+        type="button"
+        onClick={() => {
+          const nextTheme = currentTheme === 'light' ? 'dark' : 'light';
+          setTheme(nextTheme);
+          applyTheme(nextTheme);
+        }}
+      >
+        Toggle theme
+      </button>
+      <Background />
     </div>
   );
 }
---
+++ src/Background/Background.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Background.css';
+
+function Background() {
+  return (
+    <div>
+      Background
+    </div>
+  );
+}
+
+export default Background;
