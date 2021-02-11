c3041691965894a2c004a8e7d6f36b850dfd2a5a
Samkit Sheth <samkitsheth95@gmail.com>
2021-02-10 22:59:42 -0800

    Header Complete

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -3,8 +3,29 @@ import './Home.css';
 
 function Home() {
   return (
-    <div>
-      Home
+    <div className="Home">
+      <h1 className="hello">
+        hello!
+        <span className="wave">👋</span>
+      </h1>
+      <h2 className="tagline">
+        I&apos;m
+        {' '}
+        <span className="name"> Samkit Sheth </span>
+        , a full-stack software engineer
+        focused on building scalable web applications & beautiful interfaces
+        <br />
+        👨‍💻
+      </h2>
+      <h3 className="contact">
+        <span>Get in touch </span>
+        <span className="emojiPointer"> 👉 </span>
+        <span>
+          <a className="link" rel="noreferrer" href="mailto:samkitsheth95@gmail.com" target="_blank">
+            samkitsheth95@gmail.com
+          </a>
+        </span>
+      </h3>
     </div>
   );
 }
